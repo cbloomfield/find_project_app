448decc621dceecef9bd73fde34fc90d7987ab31
Chad Bloomfield <chadbloomfield@outlook.com>
2016-02-16 21:36:45 -0600

    added additional column to project

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215212544) do
+ActiveRecord::Schema.define(version: 20160217032812) do
 
   create_table "projects", force: :cascade do |t|
     t.text     "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "url"
   end
 
 end
